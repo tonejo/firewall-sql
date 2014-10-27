f67de2729ed578b5b38c620061910a3af6a8e1d8
chelo556 <marcelobarrera556@gmail.com>
2014-10-26 20:37:03 -0600

    Scripts de instalación de SQL Relay

---
+++ Instalación SQL Relay/1_rudiments.sh
@@ -0,0 +1,12 @@
+wget "http://downloads.sourceforge.net/project/rudiments/rudiments/0.48/rudiments-0.48.tar.gz"
+
+tar -xzvf rudiments-0.48.tar.gz
+
+cd rudiments-0.48
+
+./configure
+make
+
+make install
+
+
---
+++ Instalación SQL Relay/2_sqlrelay.sh
@@ -0,0 +1,12 @@
+wget "http://downloads.sourceforge.net/project/sqlrelay/sqlrelay/0.57/sqlrelay-0.57.tar.gz"
+
+tar -xzvf sqlrelay-0.57.tar.gz
+
+cd sqlrelay-0.57
+
+./configure
+make
+
+make install
+
+
